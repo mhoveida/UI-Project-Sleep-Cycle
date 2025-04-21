782d0589c91b42ba3b1f521811dfeac4334f616a
caulog <caulog20@wfu.edu>
2025-04-21 12:05:17 -0400

    working nav bar

--- website.py
+++ website.py
@@ -9,6 +9,26 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')   
 
+@app.route('/sleep_cycle')
+def sleep_cycle():
+   return render_template('sleep_cycle.html')   
+
+@app.route('/sleep_disruptors')
+def sleep_disruptors():
+   return render_template('sleep_disruptors.html')   
+
+@app.route('/calculator')
+def calculator():
+   return render_template('calculator.html')   
+
+@app.route('/quiz_start')
+def quiz_start():
+   return render_template('quiz_start.html')   
+
+@app.route('/quiz_question')
+def quiz_question():
+   return render_template('quiz_question.html')   
+
 
 # AJAX FUNCTIONS
  
